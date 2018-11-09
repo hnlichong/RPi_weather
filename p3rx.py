d8fa9f9878fedd2e5565fce93862088aff1ba16d
lichong.0186 <lichong.0186@bytedance.com>
2018-11-09 12:57:24 +0800

    add p3rx.py

--- p3rx.py
+++ p3rx.py
@@ -1,23 +1,47 @@
 #! /usr/bin/env python3
+from datetime import datetime
+
 import PyLora
 from led import led
+import os
+
+from utils import my_logger
+
+logger = my_logger(__name__, level="DEBUG")
+root_path = os.path.dirname(os.path.abspath(__file__))
 
 PyLora.init()
 PyLora.set_frequency(433000000)
 PyLora.enable_crc()
 
-while True:
-    PyLora.receive()   # put into receive mode
-    while not PyLora.packet_available():
-        # wait for a package
-        led.off(u'GREEN')
-    pkt = PyLora.receive_packet()
-    rssi = PyLora.packet_rssi()
-    snr = PyLora.packet_snr()
-    msg = u'packet: {} \nRSSI: {}, SNR: {}\n' .format(pkt, rssi, snr)
-    print msg
-    led.on(u'GREEN')
-    fo = open('./lora_data/rx', 'a')
-    fo.write(msg)
-    fo.close()
+def csv_log_create():
+    """ 创建日志文件 """
+    fields = ['datetime', 'RSSI', 'SNR', 'msg']
+    dir_path = os.path.join(root_path, 'lora_data')
+    os.makedirs(dir_path, exist_ok=True)
+    now = datetime.now()
+    file_name = now.strftime('%Y%m%d') + '.csv'
+    file_path = os.path.join(dir_path, file_name)
+    if not os.path.exists(file_path):
+        with open(file_path, 'w') as fw:
+            fw.write(','.join(fields) + '\n')
+    return file_path
+
+if __name__ == '__main__':
+    # 创建日志文件
+    log_file = csv_log_create()
+    while True:
+        PyLora.receive()   # put into receive mode
+        while not PyLora.packet_available():
+            # wait for a package
+            led.off(u'GREEN')
+        led.on(u'GREEN')
+        now = datetime.now()
+        rssi = PyLora.packet_rssi()
+        snr = PyLora.packet_snr()
+        msg = PyLora.receive_packet()
+        data = [now.strftime('%Y%m%d%H%M%S'), rssi, snr, msg]
+        logger.debug('datetime: {}, RSSI: {}, SNR: {}, msg: {}'.format(*data))
+        with open(log_file, 'a') as fa:
+            fa.write(','.join(data) + '\n')
 
