e679c410cdf72efdd9e55f9dbffbf7f4030a81a9
lichong.0186 <lichong.0186@bytedance.com>
2018-11-09 14:04:00 +0800

    map to str

--- p3tx.py
+++ p3tx.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
         now = datetime.now().strftime('%Y%m%d%H%M%S')
         temperatue, pressure = ms8607.get_temperature_pressure()
         humidity = ms8607.get_humidity()
-        data = [now, temperatue, humidity, pressure]
+        data = map(str, [now, temperatue, humidity, pressure])
         logger.debug('datetime: {}, temperatue: {}, humidity: {}, pressure: {}'.format(*data))
         msg = ','.join(data) + '\n'
         with open(log_file, 'a') as fa:
